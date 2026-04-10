ffd9c3b77416c978ea11face47421afec4cd9585
K-yoon03 <yoonnyang1013@gmail.com>
2026-04-10 13:53:15 +0900

    Fix: vite.config.ts Syntax error fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/resume/'
+  base: '/resume/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
